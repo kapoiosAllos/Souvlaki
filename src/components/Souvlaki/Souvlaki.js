a1254a410c4d9b4c0c55943fa70bc6566caf9ae1
kapoiosAllos <kyriakos.z@yahoo.com>
2018-01-19 13:17:42 +0100

    Added dynamic mapping to ingredients

--- src/components/Souvlaki/Souvlaki.js
+++ src/components/Souvlaki/Souvlaki.js
@@ -5,12 +5,18 @@ import classes from './Souvlaki.css'
 import SouvlakiIngredient from './SouvlakiIngredient/SouvlakiIngredient';
 
 const souvlaki = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingrKey => {
       return [...Array(props.ingredients[ingrKey])].map((_, index) => {
-        return <SouvlakiIngredient key = {ingrKey + 1} type = {ingrKey} />
+        return <SouvlakiIngredient key = {ingrKey + index} type = {ingrKey} />
       });
-    });
+    })
+    .reduce((previous, next) => {
+      return previous.concat(next)
+    }, []);
+    if (transformedIngredients.length === 0){
+      transformedIngredients = <p> Please start adding ingredients!</p>;
+    }
   return (
     <div className = {classes.Souvlaki}>
       <SouvlakiIngredient type="bread-top" />
