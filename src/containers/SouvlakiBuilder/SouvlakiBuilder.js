36dbf5ada10faae99809b174e183cd1020de8421
kapoiosAllos <kyriakos.z@yahoo.com>
2018-01-21 10:39:06 +0100

    Added Order button

--- src/containers/SouvlakiBuilder/SouvlakiBuilder.js
+++ src/containers/SouvlakiBuilder/SouvlakiBuilder.js
@@ -20,7 +20,19 @@ class SouvlakiBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchaseable: false
+  }
+
+  updatePurchaseState (ingredients){
+    const sum = Object.keys(ingredients)
+    .map(igKey => {
+      return ingredients[igKey];
+    })
+    .reduce((sum, el) => {
+      return sum +el;
+    }, 0);
+    this.setState({purchaseable: sum > 0})
   }
 
   addIngredientHandler = (type) => {
@@ -34,6 +46,7 @@ class SouvlakiBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -51,6 +64,7 @@ class SouvlakiBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    this.updatePurchaseState(updatedIngredients);
   }
 
   render () {
@@ -67,6 +81,7 @@ class SouvlakiBuilder extends Component {
           ingredientAdded = {this.addIngredientHandler}
           ingredientRemoved = {this.removeIngredientHandler}
           disabled = {disabledInfo}
+          purchaseable = {this.state.purchaseable}
           price = {this.state.totalPrice}
           />
       </Aux>
