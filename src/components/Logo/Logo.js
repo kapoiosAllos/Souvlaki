fea556e70b2c60b93665a7bcadf0f3c150b26e3a
kapoiosAllos <kyriakos.z@yahoo.com>
2018-01-23 08:25:59 +0100

    Changed logo and nav in mobile

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import souvlakiLogo from '../../assets/images/Recipe-Souvlaki.png';
 import classes from './Logo.css'
 
 const logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style= {{height: props.height}}>
     <img src={souvlakiLogo} alt="My Souvlaki"/>
   </div>
 );
