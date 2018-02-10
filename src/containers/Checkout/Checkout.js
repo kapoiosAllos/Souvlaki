2d313a821c2d93186b01f865a74858ac0c425cc7
kapoiosAllos <kyriakos.z@yahoo.com>
2018-02-10 07:00:32 +0100

    Added real state in continue with a search param method

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,8 +12,17 @@ class Checkout extends Component {
         }
     }
 
+    componentDidMount() {
+      const query = new URLSearchParams(this.props.location.search);
+      const ingredients = {};
+      for (let param of query.entries()) {
+        ingredients[param[0]] = +param[1];
+      }
+      this.setState({ingredients: ingredients})
+    }
+
     checkoutCancelled = () => {
-      this.props.history.goback();
+      this.props.history.goBack();
     }
 
     checkoutContinued = () => {
