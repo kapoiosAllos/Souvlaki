e37fb15ce3aa05611ed228a12cbbc465c95512d2
kapoiosAllos <kyriakos.z@yahoo.com>
2018-02-11 21:38:56 +0100

    Order submission and passed to the order

--- src/containers/SouvlakiBuilder/SouvlakiBuilder.js
+++ src/containers/SouvlakiBuilder/SouvlakiBuilder.js
@@ -89,34 +89,12 @@ class SouvlakiBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    // this.setState({loading: true})
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Kyriakos',
-    //     address: {
-    //       street: 'somewhere',
-    //       zipcode: '1106ds',
-    //       country: 'Netherlands'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    // .then(response => {
-    //   this.setState({loading: false, purchasing: false });
-    // })
-    // .catch(error => {
-    //   this.setState({loading: false, purchasing: false });
-    // });
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-     const queryString = queryParams.join('&');
-
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
       search: '?' + queryString
