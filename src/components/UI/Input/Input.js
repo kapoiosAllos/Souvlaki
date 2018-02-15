fef9bd7289cf2a9e08ec35d62da88a0e6d5244c2
kapoiosAllos <kyriakos.z@yahoo.com>
2018-02-15 16:25:31 +0100

    Added error message

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,6 +10,15 @@ const input = (props) => {
       inputClasses.push(classes.Invalid);
   }
 
+  let validationError = null;
+  if (props.invalid && props.touched) {
+    validationError = (
+      <p className={classes.ValidationError}>
+      Please enter a valid {props.valueType}
+      </p>
+    )
+  }
+
   switch (props.elementType) {
     case('input'):
       inputElement = <input
@@ -50,6 +59,7 @@ const input = (props) => {
     <div className={classes.Input}>
       <label className={classes.Label}>{props.label}</label>
       {inputElement}
+      {validationError}
     </div>
   );
 };
