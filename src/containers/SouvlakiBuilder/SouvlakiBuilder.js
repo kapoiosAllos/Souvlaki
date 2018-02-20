d07e2a186eb08229b32854cce76dc001852a442a
kapoiosAllos <kyriakos.z@yahoo.com>
2018-02-20 20:48:20 +0100

    Ingredients state now works through redux

--- src/containers/SouvlakiBuilder/SouvlakiBuilder.js
+++ src/containers/SouvlakiBuilder/SouvlakiBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Aux from '../../hoc/Aux/Aux';
 import Souvlaki from '../../components/Souvlaki/Souvlaki';
@@ -8,6 +9,7 @@ import OrderSummary from '../../components/Souvlaki/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axiosOrders';
+import * as actionTypes from '../../store/actions';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -18,7 +20,6 @@ const INGREDIENT_PRICES = {
 
 class SouvlakiBuilder extends Component {
   state = {
-    ingredients: null,
     totalPrice: 4,
     purchaseable: false,
     purchasing: false,
@@ -103,7 +104,7 @@ class SouvlakiBuilder extends Component {
 
   render () {
     const disabledInfo = {
-      ...this.state.ingredients
+      ...this.props.ings
     };
     for (let key in disabledInfo) {
       disabledInfo[key] = disabledInfo[key] <= 0
@@ -111,13 +112,13 @@ class SouvlakiBuilder extends Component {
     let orderSummary = null;
     let souvlaki = this.state.error ? <p> Ingredients cant be displayed </p> : <Spinner/>;
 
-    if (this.state.ingredients) {
+    if (this.props.ings) {
       souvlaki = (
         <Aux>
-            <Souvlaki ingredients = {this.state.ingredients} />
+            <Souvlaki ingredients = {this.props.ings} />
           <BuildControls
-            ingredientAdded = {this.addIngredientHandler}
-            ingredientRemoved = {this.removeIngredientHandler}
+            ingredientAdded = {this.props.onIngredientAdded}
+            ingredientRemoved = {this.props.onIngredientRemoved}
             disabled = {disabledInfo}
             purchaseable = {this.state.purchaseable}
             ordered = {this.purchaseHandler}
@@ -126,7 +127,7 @@ class SouvlakiBuilder extends Component {
         </Aux>
       );
       orderSummary = <OrderSummary
-      ingredients={this.state.ingredients}
+      ingredients={this.props.ings}
       price={this.state.totalPrice}
       purchaseCancelled={this.purchaseCancelHandler}
       purchaceContinued={this.purchaseContinueHandler}/>;
@@ -146,4 +147,16 @@ class SouvlakiBuilder extends Component {
   }
 }
 
-export default withErrorHandler(SouvlakiBuilder, axios);
+const mapsStateToProps = state => {
+  return {
+    ings: state.ingredients
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIngredientAdded: (ingName) =>dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
+    onIngredientRemoved: (ingName) =>dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+  };
+}
+export default connect(mapsStateToProps, mapDispatchToProps)(withErrorHandler(SouvlakiBuilder, axios));
