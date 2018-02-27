c2983cbbb9ec4da7771212f2c5743f7018917964
kapoiosAllos <kyriakos.z@yahoo.com>
2018-02-27 22:26:02 +0100

    Added authentication form and route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout'
 import SouvlakiBuilder from './containers/SouvlakiBuilder/SouvlakiBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   // state = {
@@ -24,6 +25,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" exact component={SouvlakiBuilder} />
           </Switch>
         </Layout>
