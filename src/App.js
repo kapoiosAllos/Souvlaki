b4bd52dd025cb6c2e8d1a7ae2334be268f9c02f1
kapoiosAllos <kyriakos.z@yahoo.com>
2018-02-28 12:03:15 +0100

    Added logout function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SouvlakiBuilder from './containers/SouvlakiBuilder/SouvlakiBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
   // state = {
@@ -26,6 +27,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={SouvlakiBuilder} />
           </Switch>
         </Layout>
