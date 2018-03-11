de7d9ac8d5bc8b4c03d457b7f659fa4b03625546
kapoiosAllos <kyriakos.z@yahoo.com>
2018-03-11 14:48:23 +0100

    Added saga for logic in checkAuthTimeout

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -30,13 +30,18 @@ export const logout = () => {
   return {
     type: actionTypes.AUTH_INITIATE_LOGOUT
   };
-}
+};
+
+export const logoutSucceed = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
 
 export const checkAuthTimeout = (expirationTime) => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(logout());
-    }, expirationTime * 1000);
+  return {
+    type: actionTypes.AUTH_CHECK_TIMEOUT,
+    expirationTime: expirationTime
   };
 };
 
