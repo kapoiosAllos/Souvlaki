d8c30dacfdbc3b0e77b02a557a2bb91bcf0e0181
kapoiosAllos <kyriakos.z@yahoo.com>
2018-03-11 21:39:29 +0100

    Added auto sign in through saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -39,6 +39,26 @@ export function* authUserSaga(action) {
     yield put(actions.authSuccess(response.data.idToken, response.data.localId));
     yield put(actions.checkAuthTimeout(response.data.expiresIn));
   } catch(err) {
-    yield put (actions.authFail(err.response.data.error));
-  }  
+    yield put(actions.authFail(err.response.data.error));
+  }
 }
+
+export function* authCheckStateSaga(action) {
+  const token = yield localStorage.getItem('token');
+  if (!token) {
+    yield put(actions.logout());
+  } else {
+    const expirationDate = yield new Date (localStorage.getItem('expirationDate'));
+    if (expirationDate <= new Date()) {
+      yield put(actions.logout());
+    } else {
+      const userId = yield localStorage.getItem('userId');
+      yield put(actions.authSuccess(token, userId));
+      yield put(
+        actions.checkAuthTimeout(
+          (expirationDate.getTime() - new Date().getTime()) / 1000
+        )
+      );
+    }
+  }
+};
