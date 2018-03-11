5ae440c28a9de4a7e2d797edd5ac9c275fa97403
kapoiosAllos <kyriakos.z@yahoo.com>
2018-03-11 13:17:56 +0100

    Created the sagaMiddleware and dispatched the logout action

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,12 +1,12 @@
-import { put } from 'readux-saga/effetcts';
+import { put } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 
-function* logout(action) {
+export function* logoutSaga(action) {
   yield localStorage.removeItem('token');
   yield localStorage.removeItem('expirationDate');
   yield localStorage.removeItem('userId');
   yield put({
     type: actionTypes.AUTH_LOGOUT
-  })
+  });
 }
